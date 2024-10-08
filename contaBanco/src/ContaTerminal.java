4d1cedcd3ec87268b46e3d447069aa01c50e35c3
dnvpimenta <dnvpimenta@gmail.com>
2024-10-08 17:00:41 -0300

    Finalizado1

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+    int numero;
+    String agencia;
+    String nomeCliente;
+    double saldo;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+     ContaTerminal conta = new ContaTerminal();
+        try (Scanner Scanner = new Scanner(System.in).useLocale(Locale.getDefault())) {
+            System.out.println("Digite o número da conta:");
+            conta.numero = Scanner.nextInt();
+            
+            System.out.println("Digite a Agência:");
+            conta.agencia = Scanner.next();
+            
+            System.out.println("Digite o nome do cliente:");
+            conta.nomeCliente = Scanner.next();
+            
+            System.out.println("Digite o saldo:");
+            conta.saldo = Scanner.nextDouble();
+            
+            System.out.println(
+                    "Olá " + conta.nomeCliente + ", obrigado por abrir uma conta conosco, sua agência é " + conta.agencia +
+                            ", conta " + conta.numero +
+                            " e seu saldo atual é" + conta.saldo + ", e já está disponível para saque!"
+            );
+        }
     }
 }
